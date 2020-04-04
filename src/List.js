b90536602a59e135ba0ae524b434ac05b12fc478
Hailee Miu <haileemiu@Hailees-MacBook-Pro.local>
2020-04-04 11:19:09 -0500

    list item

--- src/List.js
+++ src/List.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 const data = [
-    { 'name': 'pizza', 'completed': false, },
-    { 'name': 'spagetti', 'completed': true, },
+    { 'name': 'pizza', 'completed': false, 'amount': 2},
+    { 'name': 'spagetti', 'completed': true, 'amount': 10},
 ]
+
+const ListItem = ({item}) => {
+    return(
+        <p>{item.name}</p>
+    )
+}
 
 
 const List = () => {
@@ -11,10 +17,9 @@ const List = () => {
         <>
         <h4>List</h4>
         {data.map(item => (
-            <p>{item.name}</p>
+            <ListItem item={item}/>
         ))}
         </>
     )
 }
-
 export default List;
